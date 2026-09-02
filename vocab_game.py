a35d7bd540f37f9c8368a203fdd03330373c724d
404-07-patworakit <54285@yupparaj.ac.th>
2026-09-02 16:23:11 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -12,17 +12,18 @@ if "ans3_val" not in st.session_state:
 if "ans4_val" not in st.session_state:
     st.session_state.ans4_val = ""
 
+
 def reset_game():
-    st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
-    st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
-    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
-    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
-    st.session_state.start = time.time()  # เริ่มเวลาใหม่
-    st.session_state.is_ended = False  # ปิด Dialog
+    st.session_state.ans1_val = ""
+    st.session_state.ans2_val = ""
+    st.session_state.ans3_val = ""
+    st.session_state.ans4_val = ""
+    st.session_state.start = time.time()
+    st.session_state.is_ended = False
 
 
 st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
@@ -55,7 +56,6 @@ def show_result_dialog(ans1, ans2):
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
-
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
     if score == 4:
@@ -66,6 +66,29 @@ def show_result_dialog(ans1, ans2):
 
 st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 
+st.divider()
+ans1 = st.text_input(
+    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    value=st.session_state.ans1_val,
+)
+ans2 = st.text_input(
+    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    value=st.session_state.ans2_val,
+)
+ans3 = st.text_input(
+    "ข้อ 3: Monkeys love to eat `b a _ _ _ a`. 🍌",
+    value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+    "ข้อ 4: People love to eat `s t _ a _ b e _ _ y`. 🍓",
+    value=st.session_state.ans4_val,
+)
+
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
+
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     time_left = int(30 - (time.time() - st.session_state.start))
 
@@ -75,37 +98,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
         st.session_state.is_ended = True
         st.rerun()
 
-st.divider()
-
-ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-    value=st.session_state.ans1_val,
-)
-ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
-    value=st.session_state.ans2_val,
-)
-
-ans3 = st.text_input(
-    "ข้อ 3: Monkeys love to eat `b a _ _ _ a`. 🍌",
-    value=st.session_state.ans2_val,
-)
-
-ans4 = st.text_input(
-    "ข้อ 4: People love to eat `s t _ a _ b e _ _ y`. 🍓",
-    value=st.session_state.ans2_val,
-)
-
-
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-
-
-
-if "start" in st.session_state and not st.session_state.get("is_ended", False):
-    if st.button("📥 ส่งคำตอบ"):
+    if st.button("📤 ส่งคำตอบ"):
         st.session_state.is_ended = True
         st.rerun()
 
@@ -116,4 +109,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
-st.write("นายปรัชญ์วรกิตติ์ จันทรุทัยณัช เลขที่ 7 ม.4/4")
+st.write("นายปรัชญ์วรกรกิตติ์ จันทรหทัยณัช เลขที่ 7 ม.4/4")
