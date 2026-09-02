3ea8e2e37bab54b3ad3c9b3e9a4bdbd67b37e580
404-07-patworakit <54285@yupparaj.ac.th>
2026-09-02 16:13:36 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -113,7 +113,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
     st.rerun()
 
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2,ans3,ans4)
 
 st.divider()
 st.write("นายปรัชญ์วรกิตติ์ จันทรุทัยณัช เลขที่ 7 ม.4/4")
